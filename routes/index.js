b896aa472e64836d5d1de99d5a8f2bc3b1cb2d7f
George Bao Lee <gleeb@ocf.berkeley.edu>
2017-03-07 00:04:12 -0800

    Fix projects href, add projects view

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,8 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { name: 'ngnl' });
 });
+router.get('/projects', function(req, res, next) {
+  res.render('projects');
+});
 
 module.exports = router;
